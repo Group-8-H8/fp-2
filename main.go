e3d03ec5a4c77af383259298f4223e2b1392bdd2
Oktapratama <oktapratama772@gmail.com>
2023-06-02 16:06:54 +0700

    add controller

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"fmt"
+	"github.com/Group-8-H8/fp-2.git/config"
 	"github.com/Group-8-H8/fp-2.git/db"
+	"github.com/Group-8-H8/fp-2.git/router"
 )
 
 func main() {
-	database.StartDB()
+	r := router.StartApp()
+	err := database.StartDB()
+	if err != nil {
+		fmt.Println("Error starting database: ", err)
+		return
+	}
+	r.Run(config.SERVER_PORT)
 }
