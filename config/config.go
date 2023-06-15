a60f89f6044d764e696e1c6facf085349def3cd3
Oktapratama <oktapratama772@gmail.com>
2023-06-15 09:02:26 +0700

    deploy

--- config/config.go
+++ config/config.go
@@ -1,13 +1,10 @@
 package config
 
 var (
-	SERVER_PORT = ":8080"
-
-	HOST     string = "localhost"
-	PORT     int    = 5432
-	USERNAME string = "postgres"
-	PASSWORD string = "12345"
-	DB_NAME  string = "project-akhir"
-
-	SECRET_KEY string = "_OkTaa"
+	HOST     = os.Getenv("PG_HOST")
+	PORT     = os.Getenv("PG_PORT")
+	USERNAME = os.Getenv("PG_USER")
+	PASSWORD = os.Getenv("PG_PASSWORD")
+	DB_NAME  = os.Getenv("PG_DB_NAME")
+	dialect = "postgres"
 )
